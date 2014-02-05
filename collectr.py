99c40294165355b71d125f2501efc5ab7c4816eb
Maksim Roshtshin <ragarmakhis@gmail.com>
2014-02-05 22:27:15 +0400

    under work

--- collectr.py
+++ collectr.py
@@ -1,5 +1,8 @@
 from Cocoa import *
 from Foundation import NSObject
+import os.path, re, subprocess
+import xml.etree.ElementTree as etree
+from time import sleep
  
 class collectrController(NSWindowController):
     volumesTextField = objc.IBOutlet()
@@ -8,6 +11,7 @@ class collectrController(NSWindowController):
     typeXMLTextField = objc.IBOutlet()
     typeSourceTextField = objc.IBOutlet()
     txtOnlyTextField = objc.IBOutlet()
+    logTextView = objc.IBOutlet()
  
     def windowDidLoad(self):
         NSWindowController.windowDidLoad(self)
@@ -18,6 +22,7 @@ class collectrController(NSWindowController):
         self.createTXTOnly = 0
         self.outputTxt = '/tmp/collectr.txt'
         self.volumes = NSArray.arrayWithObject_(NSURL.URLWithString_("file://localhost/Volumes/"))
+        self.outputPath = ''
 
     @objc.IBAction
     def setVolumesDialog_(self, sender):
@@ -39,13 +44,13 @@ class collectrController(NSWindowController):
         
             # Список выбранных файлов
             self.volumes = openDlg.URLs()
-            self.volumesString = ''
+            volumesString = ''
             for item in self.volumes:
                 fileName = item.lastPathComponent()
-                item = item.path()#.replace(fileName, '')
-                self.volumesString = self.volumesString + item + '; '
+                item = item.path()
+                volumesString = volumesString + item + '; '
                  
-            self.volumesTextField.setStringValue_(self.volumesString[:-2])
+            self.volumesTextField.setStringValue_(volumesString[:-2])
                
         # // Показать выбранные файлы
         # for i in self.volumes:
@@ -73,15 +78,21 @@ class collectrController(NSWindowController):
         
             # Список выбранных файлов
             # self.xmlFile = openDlg.URLs().objectAtIndex_(0).absoluteString()
-            self.xmlFile = openDlg.URLs().objectAtIndex_(0).lastPathComponent()
+            self.xmlFile = openDlg.URLs().objectAtIndex_(0)#.lastPathComponent()
 
-            self.xmlTextField.setStringValue_(self.xmlFile)
+            self.xmlTextField.setStringValue_(self.xmlFile.path())
+
+            if self.xmlFile.pathExtension() != 'xml':
+                alert = NSAlert.alloc().init()
+                alert.setMessageText_('Please select XML file')#.componentsJoinedByString_(",\n"))
+                alert.runModal()
+
         
     @objc.IBAction
     def setTypeXML_(self, sender):
         # [F]inal Cut Pro or [D]avinci Resolve
 
-        self.typeXML = self.sender.selectedItem().toolTip()
+        self.typeXML = sender.selectedItem().toolTip()
 
         self.typeXMLTextField.setStringValue_(self.typeXML)
 
@@ -92,12 +103,6 @@ class collectrController(NSWindowController):
         self.typeSource = sender.selectedItem().toolTip()
 
         self.typeSourceTextField.setStringValue_(self.typeSource)
-
-    # @objc.IBAction
-    # def setOutputTXT_(self, sender):
-    #     # Output TXT file name
-
-    #     pass
 
     @objc.IBAction
     def setPath_(self, sender):
@@ -123,7 +128,7 @@ class collectrController(NSWindowController):
 
     @objc.IBAction
     def setOutputTxt_(self, sender):
-        # Path where to copy R3D files
+        # Output TXT file name
         
         # Создать диалог
         saveDlg = NSSavePanel.savePanel()
@@ -142,10 +147,10 @@ class collectrController(NSWindowController):
             # Список выбранных файлов
             self.outputTxt = saveDlg.filename()#.objectAtIndex_(0)
 
-            alert = NSAlert.alloc().init()
-            alert.setMessageText_(self.outputTxt)#.componentsJoinedByString_(",\n"))
-            alert.runModal()
-            # self.outputTextField.setStringValue_(self.outputTxt)
+            # alert = NSAlert.alloc().init()
+            # alert.setMessageText_(self.outputTxt)#.componentsJoinedByString_(",\n"))
+            # alert.runModal()
+            self.outputTextField.setStringValue_(self.outputTxt)
 
     @objc.IBAction
     def createTXTOnly_(self, sender):
@@ -161,31 +166,114 @@ class collectrController(NSWindowController):
     @objc.IBAction
     def start_(self, sender):
         # start
-        
+
         #Set input XML filename from args
-        xmlName = self.xmlFile
+        xmlName = self.xmlFile.path()
 
         # Set text filename from args or by default
-        txtName = self.outputTxt
-        # if args.output:
-        #     txtName = args.output
-        # else: 
-        #     txtName = args.input + '.txt'
+        if self.outputTxt == '/tmp/collectr.txt':
+            txtName = '/tmp/' + self.xmlFile.lastPathComponent().replace(self.xmlFile.pathExtension(), 'txt')
+        else:
+            txtName = self.outputTxt.path()
+        
+        
 
-        # Set Volume from args or by default
-        volName = self.volumes
-        # if args.volume:
-        #     volName = '/Volumes/' + args.volume
-        # else:
-        #     volName = '/Volumes'
-        # print '\n' + 'R3D files will search in ' + volName + '...'
+        volumesString = ''
+        for item in self.volumes:
+            fileName = item.lastPathComponent()
+            item = item.path()#.replace(fileName, '')
+            volumesString = volumesString + item + '; '
+        
+        self.logTextView.insertText_('\n' + 'R3D files will search in ' + volumesString[:-2] + '...' + '\n')
 
         # Set path to copy R3D files
-        if args.path:
-            pathName = args.path + '/'
+        # pathName = self.outputPath
+        if self.outputPath == '':
+            pathName = self.xmlFile.path().replace(self.xmlFile.lastPathComponent(), '')
         else:
-            pathName = '$PWD'
-        print 'R3D files will be copied to ' + pathName
+            pathName = self.outputPath.path()
+        self.logTextView.insertText_('R3D files will be copied to ' + pathName + '\n')
+
+        #Select XML schema query
+        if self.typeXML == 'F':
+            xmlPath = ".//sequence/media/video/track/clipitem/file/name" # From Final Cut
+        else:
+            xmlPath = ".//VideoTrackVec/Element/Sm2TiTrack/Items/Element/Sm2TiVideoClip/MediaReelNumber" # From Davinci Resolve
+
+        #Regexp for search in XML
+        if self.typeSource == 'R':
+            xmlSearch = "^[A-Z]\d{3}_[A-Z]\d{3}_\d{4}\w{2}" # For R3D
+        else:
+            xmlSearch = "^[A-Z]\d{3}[A-Z]\d{3}_\d{4}\w{2}" # For Alexa
+
+        #Make text file with all R3D filenames from XML
+        fileCount = 0
+        if not os.path.exists(txtName):
+            txtfile = open(txtName, 'w+')
+            tree = etree.parse(xmlName)
+            root = tree.getroot()
+            for elem in root.findall(xmlPath):
+                match = re.match(xmlSearch, elem.text) 
+                if match:
+                    txtfile.write(elem.text + '\n')
+                    fileCount += 1
+            self.logTextView.insertText_('Found ' + str(fileCount) + ' cut(s)' + '\n')
+            txtfile.close
+            txtfile = open(txtName, 'r')
+            tempurls = txtfile.readlines()
+            pathurls = list(set(tempurls))
+            fileCount = len(pathurls)
+            txtfile.close
+            self.logTextView.insertText_('Found ' + str(fileCount) + ' file(s)' + '\n')
+        else:
+            self.logTextView.insertText_('Text file already exists, reading contents...' + '\n')
+
+
+
+        if self.createTXTOnly != 'YES':
+            sleep(3)
+
+            #Open text file and read content
+
+            txtfile = open(txtName, 'r')
+            tempurls = txtfile.readlines()
+            pathurls = list(set(tempurls))
+            fileCount = len(pathurls)
+            txtfile.close
+
+            lineCount = 1
+            R3Dname = ''
+
+            #Find and copy R3D files
+            for item in self.volumes:
+                volName = item.path()
+                txtfile = open(txtName, 'w+')
+                localFileManager = NSFileManager.alloc().init()
+                dirEnumerator = localFileManager.enumeratorAtURL_includingPropertiesForKeys_options_errorHandler_(directoryToScan, NSArray.arrayWithObjects_(NSURLNameKey, NSURLIsDirectoryKey, None), NSDirectoryEnumerationSkipsHiddenFiles, None)
+                theArray = NSMutableArray.array()
+                for filename in pathurls:
+                    for theURL in dirEnumerator:
+                        if self.typeSource == 'R':
+                            R3Dname = filename[0:16] + '.RDC' # RED
+                            theURL.getResourceValue_forKey_error_(&fileName, NSURLNameKey, None)
+                            # finded = subprocess.check_output(['find', volName, '-type', 'd', '-name', R3Dname]) # RED
+                        else:
+                            R3Dname = filename[0:20] + '*' + '.mov' # Alexa
+                            theURL.getResourceValue_forKey_error_(&fileName, NSURLNameKey, None)
+                            # finded = subprocess.check_output(['find', volName, '-type', 'f', '-name', R3Dname]) 
+
+                    if finded:
+                        self.logTextView.insertText_(filename.rstrip('\n') + ' --------------- ' + str(lineCount) + ' of ' + str(fileCount) + '\n')
+                        subprocess.call(['cp', '-R', finded.rstrip(), pathName])
+                        self.logTextView.insertText_(finded + '\n')
+                        lineCount += 1
+                    else:
+                        txtfile.write(filename)
+                    
+        elif self.createTXTOnly == 'YES':
+            return 0
+
+        self.logTextView.insertText_('Found and copied ' + str(lineCount-1) + ' file(s) of ' + str(fileCount) + '\n')
  
     # def updateDisplay(self):
     #     self.counterTextField.setStringValue_(self.count)
